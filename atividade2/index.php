fa0624352c1e26c8f5b92a84e1e46826a2230c46
Laís Boscolo <costaboscololais2008@gmail.com>
2024-10-29 16:22:05 -0300

    Atividade 2

---
+++ atividade2/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// idade =caixinha variavel
+    $idade = 12;
+
+    // condiconal
+
+    if ($idade < 18 ){
+
+        echo "<p> Acesso Negado </p>";
+
+    }else{
+
+        echo "<p>Acesso Permitido</p>";
+
+
+    }
+
+?>
