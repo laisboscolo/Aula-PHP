a7754f2bf76e3a7b28012c98eba866b696ebc6f6
Laís Boscolo <costaboscololais2008@gmail.com>
2024-11-05 09:16:10 -0300

    atividade 6

---
+++ atividade6/index.php
@@ -0,0 +1,28 @@
+<?php
+    $dia = 3;
+    switch($dia){
+        case 1:
+            echo "domingo";
+            break;
+        case 2:
+            echo "segunda";
+            break;
+        case 3:
+            echo "terça";
+            break;
+        case 4:
+            echo "quarta";
+            break;
+        case 5:
+            echo "quinta";
+            break;
+        case 6:
+            echo "sexta";
+            break;
+        case 7:
+            echo "sábado";
+            break;
+        default:
+            echo "Escolha um número de 1 a 7";
+    }
+?>
